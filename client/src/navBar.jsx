beefcc530f5fe134604c74c5ac4e04e1760dcf3a
ghildreth <gabrielantonhildreth@gmail.com>
2018-04-19 01:49:01 +0000

    added an icon and more font styles

--- client/src/navBar.jsx
+++ client/src/navBar.jsx
@@ -3,7 +3,9 @@ import React, {Component} from 'react';
 class NavBar extends Component {
   render(){
     return(
+
     <nav className="navbar">
+    <i className="far fa-comment-alt"></i>
       <a href="/" className="navbar-brand">Chatty</a>
       {this.props.userCount > 1 ? <span> Users online: {this.props.userCount}</span> : <span> {this.props.userCount} User online: </span>}
     </nav>
